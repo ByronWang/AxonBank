c1fb35ab659af4ec6fb0486e555dfe3220bd3272
Byron Wang <wangshilian@gmail.com>
2017-05-04 11:59:52 +0800

    remove unused data

--- core/src/test/java/com/nebula/dropwizard/core/TodoTest.java
+++ core/src/test/java/com/nebula/dropwizard/core/TodoTest.java
@@ -3,11 +3,6 @@ package com.nebula.dropwizard.core;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import org.axonframework.samples.bank.api.bankaccount.BankAccountMoneyDepositCommand;
-import org.axonframework.samples.bank.api.bankaccount.BankAccountMoneyDepositedEvent;
-import org.axonframework.samples.bank.api.bankaccount.BankAccountMoneySubtractedEvent;
-import org.axonframework.samples.bank.simple.instanceCommand.BankAccountInstance;
-import org.junit.Test;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.util.ASMifier;
@@ -15,83 +10,9 @@ import org.objectweb.asm.util.TraceClassVisitor;
 
 public class TodoTest {
 
-//	@Test
-//	public void testTodo() throws IOException {
-//		ClassReader cr = new ClassReader(Todo.class.getName());
-//		ClassVisitor typemaker = new CQRSBuilder(Opcodes.ASM5);
-//		cr.accept(typemaker, ClassReader.SKIP_DEBUG);
-//
-//		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
-//		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-//
-//		System.out.println(typemaker.toString());
-//	}
-	@Test
-	public void testPrintTodo() throws IOException {
+	public static void main(String[] args) throws IOException {
 		ClassReader cr = new ClassReader(Todo.class.getName());
 		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
 		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-	}
-
-	@Test
-	public void testPrintBankAccount() throws IOException {
-		ClassReader cr = new ClassReader(BankAccountInstance.class.getName());
-		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
-		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-	}
-	
-	
-	@Test
-	public void testPrintcommand_BankAccount() throws IOException {
-		ClassReader cr = new ClassReader(org.axonframework.samples.bank.command.BankAccount.class.getName());
-		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
-		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-	}
-	
-	
-	@Test
-	public void testPrintBankAccountMoneyDepositCommand() throws IOException {
-		ClassReader cr = new ClassReader(BankAccountMoneyDepositCommand.class.getName());
-		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
-		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-	}
-
-	@Test
-	public void BankAccountMoneyDepositedEvent() throws IOException {
-		ClassReader cr = new ClassReader(BankAccountMoneyDepositedEvent.class.getName());
-		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
-		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-	}
-	
-	@Test
-	public void testPrintBankAccountMoneySubtractedEvent() throws IOException {
-		ClassReader cr = new ClassReader(BankAccountMoneySubtractedEvent.class.getName());
-		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
-		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-	}
-	
-	@Test
-	public void testPrintBankAccountMoneyDepositedEvent() throws IOException {
-		ClassReader cr = new ClassReader(BankAccountMoneyDepositedEvent.class.getName());
-		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
-		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-	}
-
-
-	@Test
-	public void testPrintTodo2() throws IOException {
-		ClassReader cr = new ClassReader(Todo2.class.getName());
-		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(System.out));
-		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
-	}
-
-	static class MyClassLoader extends ClassLoader {
-		public Class<?> defineClass(String name, byte[] b) {
-			return defineClass(name, b, 0, b.length);
-		}
-		
-		public void doResolveClass(Class<?> clz){
-			super.resolveClass(clz);
-		}
 	}
 }
